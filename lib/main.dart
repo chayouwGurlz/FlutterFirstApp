f278743be02f59b96c836e5f276cfc3ad1a7593e
Irine Octaviana <chayouw_gurlz@yahoo.com>
2020-05-23 12:59:46 +0700

    - try reformat code with dartfmt
    - use column as widget container
    - give any feedback from press button

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,45 @@
- import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
- /*void  main (){
+/*void  main (){
    runApp(MyApp());
  }*/
 
- void main () => runApp(MyApp());
+void main() => runApp(MyApp());
 
- class MyApp extends StatelessWidget{
-   @override
-  Widget build(BuildContext context){
-    return MaterialApp(home: Scaffold(
-      appBar: AppBar(title: Text('My First App'),
+class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer chosen!');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color ?',
+      'What\'s your favorite animal ?'
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Text('The question!'),
+            RaisedButton(
+              child: Text('Answer1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer2'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer3'),
+              onPressed: answerQuestion,
+            ),
+          ],
+        ),
       ),
-      body: Text('This is my default text!'),
-    ),
     );
   }
- }
+}
